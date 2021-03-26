af52dd7232fb9c63c7cda485b23367de3a6e4187
kremrik <kyle.robert.e@gmail.com>
2021-03-26 15:55:12 -0500

    test for positional path arg

--- tests/functional/test_path_positional_arg/test_path_positional_arg.py
+++ tests/functional/test_path_positional_arg/test_path_positional_arg.py
@@ -1,37 +1,34 @@
 from tests.functional.utils import file_contents
 
 import unittest
-from os import chdir, environ
+from os import chdir
 from os.path import dirname
 from shutil import copyfile
 from subprocess import Popen
 from time import sleep
 
 
-class test_change_headings_decrease(unittest.TestCase):
+class test_inject_code(unittest.TestCase):
     def setUp(self) -> None:
         chdir(dirname(__file__))
-        environ["HRM_PLUGINS"] = "plugins"
-
         copyfile("README.bak", "README.md")
-        copyfile("sub_dir/README.bak", "sub_dir/README.md")
+        copyfile("subdir/README.bak", "subdir/README.md")
 
     def tearDown(self) -> None:
-        del environ["HRM_PLUGINS"]
         copyfile("README.bak", "README.md")
-        copyfile("sub_dir/README.bak", "sub_dir/README.md")
+        copyfile("subdir/README.bak", "subdir/README.md")
 
     def test(self):
-        cmd = "hrm test-plugin --change 1 -v".split()
+        cmd = "hrm inject-code subdir -v".split()
         Popen(cmd)
 
         sleep(0.15)
 
         md1_output = file_contents("README.md")
-        md2_output = file_contents("sub_dir/README.md")
+        md2_output = file_contents("subdir/README.md")
 
         md1_gold = file_contents("README.gold")
-        md2_gold = file_contents("sub_dir/README.gold")
+        md2_gold = file_contents("subdir/README.gold")
 
         self.assertEqual(md1_output, md1_gold)
         self.assertEqual(md2_output, md2_gold)
